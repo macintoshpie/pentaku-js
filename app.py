fe58a3e5cbec69843d3b24ff34bdda389f5ed051
macintoshpie <ted.summer2@gmail.com>
2017-10-07 22:21:24 -0400

    cleaned up CSS

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template, send_file
-#app = Flask(__name__, static_url_path='')
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-	#return send_file('index.html')
+def index():
 	return render_template('index.html')
-    #return "Hello World!"
 
 if __name__ == '__main__':
     app.run()
-    #app.debug = True
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
